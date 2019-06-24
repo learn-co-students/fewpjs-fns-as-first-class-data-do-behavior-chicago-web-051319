bafaf768a0939d64a493db5f92355f714a245aeb
afhare <alexandra.f.hare@gmail.com>
2019-06-24 11:01:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { endianness } from "os";
+
 /* Given Code, don't edit */
 
 function handleClick(e) {
@@ -8,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hours = parseInt(time.split(":")[0]);
+    if(hours < 12){
+      return "Good Morning";
+    }
+    else if (hours > 12 && hours < 17){
+      return "Good Afternoon";
+    }
+    else {
+      return "Good Evening";
+    }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(text) {
+  let h1 = document.getElementById('greeting');
+  h1.innerText = text;
+}
